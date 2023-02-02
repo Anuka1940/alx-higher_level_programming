66d66072fa1d5379bf5131240dd1dde5fcfc6041
Abdul Lateef Anuka <anukalateef@gmail.com>
2023-02-02 15:54:11 -0800

    rearrange objects

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -10,6 +10,19 @@ class Rectangle:
         self.__height = height
 
     @property
+    def width(self):
+        return self.__width
+
+    @height.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
     def height(self):
         return self.__height
 
@@ -21,16 +34,3 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         else:
             self.__height = value
-
-    @property
-    def width(self):
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        if type(value) != int:
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
