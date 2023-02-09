d94d4bbffa807379372ee932dcc526c323d2126d
Abdul Lateef Anuka <anukalateef@gmail.com>
2023-02-09 14:34:56 -0800

    compare if obj in an instance of a class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/urs/bin/python3
+""" Define a funtion that return True if an obj is an instance of a class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """compare if obj is from a_class
+
+    return True or False"""
+    return isinstance(obj, a_class)
