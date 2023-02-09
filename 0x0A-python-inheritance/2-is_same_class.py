ce53de32a712354835788af0ffc14e75c241ce7a
Abdul Lateef Anuka <anukalateef@gmail.com>
2023-02-09 14:09:52 -0800

    compare the types of obj and class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+""" This module define a fuction that check the class of an obj
+"""
+
+
+def is_same_class(obj, a_class):
+    """check for type of obj with a_class
+
+    return True if same and False other wise"""
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
