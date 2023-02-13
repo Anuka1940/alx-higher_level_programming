17cf75655fb7f0ed45bd3041addc3b26210b2452
Abdul Lateef Anuka <anukalateef@gmail.com>
2023-02-13 15:19:42 -0800

    write a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Define write_file"""
+
+
+def write_file(filename="", text=""):
+    """A function that write in a file
+    Args:
+        filename: name of the file
+        text: the written string
+    """
+    with open(filename, "w", encoding="UTF8") as f:
+        return (f.write(text))
