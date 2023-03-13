8e079cb59648bdcdd4354f94b096d852b0209b96
Abdul Lateef Anuka <anukalateef@gmail.com>
2023-03-13 12:29:30 -0700

    print integers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-
-    for i in matrix:
-        print("{:d}".format(i), end=" ")
+    for row in matrix:
+        for i in row:
+            if row.index(i) < len(row)-1:
+                print("{:d}".format(i), end=' ')
+            else:
+                print("{:d}".format(i), end='')
+        print()
