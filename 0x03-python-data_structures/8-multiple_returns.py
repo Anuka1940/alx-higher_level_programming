42432cd0436b53f903397ca01b614aa15d3ae570
Abdul Lateef Anuka <anukalateef@gmail.com>
2023-03-13 15:04:13 -0700

    length of tuple

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-        if True:
+        if sentence:
             length = len(sentence)
             return length, sentence[0]
-        return length, None
+        return len(sentence), None
