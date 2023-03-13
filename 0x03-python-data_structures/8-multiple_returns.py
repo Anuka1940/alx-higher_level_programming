dbd0f72209ce048908d9ca58ab4e6e2bb6a32248
Abdul Lateef Anuka <anukalateef@gmail.com>
2023-03-13 15:08:47 -0700

    add requirement

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,4 +3,5 @@ def multiple_returns(sentence):
         if sentence:
             length = len(sentence)
             return length, sentence[0]
-        return len(sentence), None
+        elif len(sentence) == 0:
+            return len(sentence), None
