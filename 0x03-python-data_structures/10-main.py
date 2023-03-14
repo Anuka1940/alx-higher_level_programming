3ae64baa231638a2e599d18727c4514a7f652508
Abdul Lateef Anuka <anukalateef@gmail.com>
2023-03-14 02:25:25 -0700

    fix pycodestyle

--- 0x03-python-data_structures/10-main.py
+++ 0x03-python-data_structures/10-main.py
@@ -8,3 +8,5 @@ i = 0
 while i < len(list_result):
     print("{:d} {:s} divisible by 2".format(my_list[i], "is" if list_result[i] else "is not"))
     i += 1
+print(len(list_result))
+print(len(my_list))
