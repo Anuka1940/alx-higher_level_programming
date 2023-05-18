4896806792a694cd57a39d948c8f95d140d95cd1
Abdul Lateef Anuka <anukalateef@gmail.com>
2023-05-18 10:23:58 -0700

    fix error

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,8 @@
--- Convert datebase to UTF8
+-- Convert datebase to UTF8;
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- convert the table to UTF8
 ALTER TABLE first_table CONVERT TO CHARACTER SET  utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Convert the specific field/column to UTF8
+ALTER TABLE hbtn_0c_0.first_table MODIFY COLUMN name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
