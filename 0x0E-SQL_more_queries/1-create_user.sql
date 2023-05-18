090520a5a47dda67277fbaa999bbff4c3dfad065
Abdul Lateef Anuka <anukalateef@gmail.com>
2023-05-18 15:05:01 -0700

    Root user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- Create the MySQL server user user_0d_1
+DROP USER IF EXISTS 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
