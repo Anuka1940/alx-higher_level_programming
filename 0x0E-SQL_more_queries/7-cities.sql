7e36620e243b18b3117c0183e2bb820e98ec241a
Abdul Lateef Anuka <anukalateef@gmail.com>
2023-05-20 12:10:05 -0700

    cities table

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- Create database and table cities
+CREATE DATABASE IF NOT EXISTS  hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+	id INTEGER UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(256) NOT NULL
+);
+	
