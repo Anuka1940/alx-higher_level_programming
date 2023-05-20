62caa0101ebddb5abc8b54a8760a3c90405b0923
Abdul Lateef Anuka <anukalateef@gmail.com>
2023-05-20 14:38:41 -0700

    cities by states

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,8 @@
+-- List all the cities in the database
+-- Display :cities.id - cities.name - states.name
+-- Results must be sorted in ascending order by cities.id
+SELECT c.id, c.name, s.name
+FROM state AS s
+JOIN cities AS c
+ON c.state_id = s.id
+ORDER BY c.id ASC;
