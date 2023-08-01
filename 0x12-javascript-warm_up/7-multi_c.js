968306536aba696818fdb0380d78a6a48df4a9ab
Abdul Lateef Anuka <anukalateef@gmail.com>
2023-08-01 13:12:46 -0700

    I love C

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const arg = parseInt(process.argv[2]);
+let i = 0;
+if (isNaN(arg))  {
+	console.log('Missing number of occurrences')
+}
+while ( i < arg) {
+	console.log('C is fun');
+	i++;
+}
+
+
