7b67db8d08ca5bb2748f4b6c35bd1932a301ec75
Abdul Lateef Anuka <anukalateef@gmail.com>
2023-08-01 02:57:53 -0700

    fix error

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,8 +2,8 @@
 
 const arg = parseInt(process.argv[2]);
 
-if (!isNaN(parseInt(arg))) {
-    console.log('My number:' + arg);
+if (!isNaN(arg)) {
+    console.log('My number: ' + arg);
 } else {
     console.log('Not a Number');
 }
