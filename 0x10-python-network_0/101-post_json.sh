003502434206533aa330c04735d746b21c18c510
Abdul Lateef Anuka <anukalateef@gmail.com>
2023-08-31 15:39:08 -0700

    add cat commend

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a POST request with the content of a file passed with the filename as the second argument of the scrip
-$(curl -X POST -H "Content-Type: application/json" -d "$2" -s "$1")
+$(curl -X POST -H "Content-Type: application/json" -d "$(cat $2)" -s "$1")
