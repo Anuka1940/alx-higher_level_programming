2e28bcd775ac7b08f3126dbcdaef589668c89379
Abdul Lateef Anuka <anukalateef@gmail.com>
2023-08-31 09:51:43 -0700

    fix requirement

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 # use curl to send a request to a url and get the size of the response in bytes
-
-if [ $# -eq 0 ]; then
-	echo "Usage: $0 <URL>"
-	exit 1
-fi
-url="$1"
-response=$(curl -s "$url")
-content_length=$(echo -n "$response" | wc -c)
-
-if [ -n "$content_length" ]; then
-	echo "$content_length"
-else
-	echo "Unable to retrieve content length"
-fi
+[ $# -eq 0 ] && echo "Usage: $0 <URL>" && exit 1; url="$1"; echo "$(curl -s "$url" | wc -c)"
