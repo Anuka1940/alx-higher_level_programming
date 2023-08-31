ef8888f779e3191eba33896acb856e5e6f560814
Abdul Lateef Anuka <anukalateef@gmail.com>
2023-08-31 09:25:09 -0700

    cURL body size

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# use curl to send a request to a url and
+# get the size of the response in bytes
+
+if [ $# -eq 0 ]; then
+	echo "Usage: $0 <URL>"
+	exit 1
+fi
+url="$1"
+response=$(curl -s "$url")
+content_length=$(echo -n "$response" | wc -c)
+
+if [ -n "$content_length" ]; then
+	echo "$content_length"
+else
+	echo "Unable to retrieve content length"
+fi
