7088ed4981ecbfcf319c4feef5dcfa01d8c159e2
Abdul Lateef Anuka <anukalateef@gmail.com>
2023-08-31 09:33:55 -0700

    fix requirement error

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# use curl to send a request to a url and
-# get the size of the response in bytes
+# use curl to send a request to a url and get the size of the response in bytes
 
 if [ $# -eq 0 ]; then
 	echo "Usage: $0 <URL>"
