636250f5d49f609fd0a6929c1f9781ee2230ae4a
Abdul Lateef Anuka <anukalateef@gmail.com>
2023-09-02 13:21:19 -0700

    Task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Fetch URLs"""
+import urllib.request
+
+url = 'https://alx-intranet.hbtn.io/status'
+with urllib.request.urlopen(url) as response:
+    html = response.read()
+try:
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        print("Body response:")
+        print("     - type:", type(body))
+        print("     - content:", body)
+        print("     - utf8 content:", body.decode('utf-8'))
+except Exception as e:
+    print("Error:", e)
