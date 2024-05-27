c1d5bcd7bc1e1eab1aea2328834be048788d17cf
Abdul Lateef Anuka <anukalateef@gmail.com>
2024-05-27 22:18:49 +0100

    Update file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,21 +2,8 @@
 
 const fs = require('fs');
 
-// Get the file path and the string to write from the command line arguments
-const filePath = process.argv[2];
-const contentToWrite = process.argv[3];
-
-if (!filePath || !contentToWrite) {
-    console.error('Please provide both a file path and a string to write.');
-    process.exit(1);
-}
-
-// Write the content to the file in utf-8 encoding
-fs.writeFile(filePath, contentToWrite, 'utf8', (err) => {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log('Content written successfully');
-    }
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (err) {
+  if (err) {
+    console.log(err);
+  }
 });
-
